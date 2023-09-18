afd0a7bfa51158dc79ec94381b6a035eaffa6ba1
krivenko dmitrij <krivenko.dmitry@gmail.com>
2023-09-18 22:10:52 +0300

    Delete file

--- db_test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
-
-
-cursor.execute('''SELECT message FROM dima_yulya_message''')
-enter = cursor.fetchall()
-print(enter)
